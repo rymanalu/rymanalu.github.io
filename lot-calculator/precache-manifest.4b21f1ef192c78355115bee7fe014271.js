907611000f17855b62034207dc7d73d47ed55049
Roni Yusuf Manalu <rymanalu@gmail.com>
2020-07-28 20:52:21 +0700

    Added lot calculator

---
+++ lot-calculator/precache-manifest.4b21f1ef192c78355115bee7fe014271.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9087ba0b568a93d52c373be94a60f165",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89cb7a61286b4f9708b8",
+    "url": "/static/js/2.e499c80a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e499c80a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e944641994045ff5fa7a",
+    "url": "/static/js/main.9c62f9d2.chunk.js"
+  },
+  {
+    "revision": "89a7d586677e19af1d77",
+    "url": "/static/js/runtime-main.46eec375.js"
+  }
+]);
