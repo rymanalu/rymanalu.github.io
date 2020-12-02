f3cba03fa19d88ee2b27361022b64e4e0717df68
Roni Yusuf Manalu <rymanalu@gmail.com>
2020-12-02 20:54:56 +0700

    Added loketh

---
+++ loketh/precache-manifest.ad6aff7ce740d0bb31e96572981b9761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1545cf7c7e037e1807a85be7156f0868",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d743bafa772475dbb970",
+    "url": "/static/css/2.43311e01.chunk.css"
+  },
+  {
+    "revision": "8c6dbfcd4abd31c1aee4",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "d743bafa772475dbb970",
+    "url": "/static/js/2.d2976415.chunk.js"
+  },
+  {
+    "revision": "8c6dbfcd4abd31c1aee4",
+    "url": "/static/js/main.0cb47739.chunk.js"
+  },
+  {
+    "revision": "cfa1adc2d55dce2d731f",
+    "url": "/static/js/runtime-main.b448cf65.js"
+  }
+]);
