6c431650a2a9673430ec758c41fc153b5b6df8cb
Roni Yusuf Manalu <rymanalu@gmail.com>
2020-12-03 09:37:26 +0700

    Update loketh

---
+++ loketh/precache-manifest.919d8769bfaa539eeb666987fce791b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67c03d75773d39e16e001ff23130f1e",
+    "url": "/loketh/index.html"
+  },
+  {
+    "revision": "d584ae726e372c3c272f",
+    "url": "/loketh/static/css/2.43311e01.chunk.css"
+  },
+  {
+    "revision": "7d4d73aff1c35d9ecc54",
+    "url": "/loketh/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "d584ae726e372c3c272f",
+    "url": "/loketh/static/js/2.816b9c55.chunk.js"
+  },
+  {
+    "revision": "7d4d73aff1c35d9ecc54",
+    "url": "/loketh/static/js/main.106ea5a0.chunk.js"
+  },
+  {
+    "revision": "4909019be8400947e491",
+    "url": "/loketh/static/js/runtime-main.4afb707a.js"
+  }
+]);
--- loketh/service-worker.js
+++ loketh/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d54b0f3de2bcbffe8d1d8970d62e43a1.js"
+  "/loketh/precache-manifest.919d8769bfaa539eeb666987fce791b1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/loketh/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
